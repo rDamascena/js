813aba96aeb038877e3c9bd41f53595bd5682b09
rDamascena <108429828+rDamascena@users.noreply.github.com>
2025-06-07 21:19:21 -0300

    Add files via upload

---
+++ untranslate.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name Untranslate
+// @namespace https://david-trapp.com/
+// @version 2023.12.14
+// @description Auto-show original of translated search results
+// @author David Trapp
+// @match https://www.google.com/search?*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict'
+
+    function untranslate() {
+        [...document.querySelectorAll('svg[viewBox="0 0 24 24"]')]
+        .reverse()
+            .map(x => x.closest('[data-ved]')?.querySelector('[jsaction]'))
+            .filter(x => x?.innerText.includes('English') && !x?.classList.contains('untranslated') && x?.closest('[jscontroller]'))
+            .forEach(x => {
+                x.closest('[jscontroller]').style.display = 'none' // Prevent scroll on .focus() and get rid of the translation box entirely
+                x.classList.add('untranslated')
+                x.click()
+            })
+    }
+
+    untranslate()
+    new MutationObserver(untranslate).observe(document.body, {
+        subtree: true,
+        childList: true
+    })
+})()
